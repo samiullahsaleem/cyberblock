ebc2ef9c6748b4bb81c1a429a599427c58af52c9
Noman Latif <75476323+nomanlatif123@users.noreply.github.com>
2022-10-07 10:19:14 +0500

    Create palindrome.py

---
+++ Python/palindrome.py
@@ -0,0 +1,23 @@
+# This program performs palindrome check for a string #
+# function which return reverse of a string 
+  
+def isPalindrome(s): 
+    # Calling reverse function 
+   if len(s) <= 1 :
+      return True
+   if s[0] == s[len(s) - 1] :
+      return isPalindrome(s[1:len(s) - 1])
+   else :
+      return False
+    
+  
+# Driver code 
+Palindrome_input_Variable = [ ' AnnA ' , ' SoloS ' , ' RotatoR ' , ' RadaR ' , ' SagaS ' , ' RotoR ' , ' TenT ' , ' RepapeR ' , ' CiviC ' , ' KayaK ' , ' Lever ' , ' MadaM ' , ' RacecaR ' , ' StatS ' , ' Redder ' , ' Wow ' , ' MoM ' , ' RefeR ' , ' NooN ']
+
+print( " PALINDROME CHECK PROGRAM " )
+for i in Palindrome_input_Variable:
+   ans = isPalindrome(i) 
+   if ans == 1: 
+    print( " The given string  ", "'" , i , "' ","is a palindrome") 
+   else: 
+    print( " The given string  " , "'" , i , "' ","is not a palindrome")
