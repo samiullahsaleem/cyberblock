04a7cec7316c1f08daf18326aa32e35766741aae
Muhammad Manan <88824142+MananDev@users.noreply.github.com>
2022-10-08 21:40:31 +0500

    HangMan C Sharp
    This is a C# Program to create a HangMan game.

---
+++ C#/hangman.cs
@@ -0,0 +1,49 @@
+/*
+ *  C# Program to Create a HangMan Game
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Console.WriteLine("Welcome to Hangman!!!!!!!!!!");
+            string[] listwords = new string[10];
+            listwords[0] = "sheep";
+            listwords[1] = "goat";
+            listwords[2] = "computer";
+            listwords[3] = "america";
+            listwords[4] = "watermelon";
+            listwords[5] = "icecream";
+            listwords[6] = "jasmine";
+            listwords[7] = "pineapple";
+            listwords[8] = "orange";
+            listwords[9] = "mango";
+            Random randGen = new Random();
+            var idx = randGen.Next(0, 9);
+            string mysteryWord = listwords[idx];
+            char[] guess = new char[mysteryWord.Length];
+            Console.Write("Please enter your guess: ");
+ 
+            for (int p = 0; p < mysteryWord.Length; p++)
+                guess[p] = '*';
+ 
+            while (true)
+            {
+                char playerGuess = char.Parse(Console.ReadLine());
+                for (int j = 0; j < mysteryWord.Length; j++)
+                {
+                    if (playerGuess == mysteryWord[j])
+                        guess[j] = playerGuess;
+                }
+                Console.WriteLine(guess);
+            }
+        }
+    }
+}
