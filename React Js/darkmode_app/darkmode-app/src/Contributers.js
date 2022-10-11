b87e637731734c81de273a502c5b9b4df4c2d9e0
Shubham_Baghel <shubhambaghel34@gmail.com>
2022-10-11 21:22:59 +0530

    added project  for darkmodefunctionality

---
+++ React Js/darkmode_app/darkmode-app/src/Contributers.js
@@ -0,0 +1,239 @@
+const Contributers = [
+  {
+    gitUrl: "https://github.com/samiullahsaleem",
+    imagrUrl: "https://avatars.githubusercontent.com/u/54318487?v=4",
+    userName: "Sami Ullah Saleem",
+  },
+  {
+    gitUrl: "https://github.com/aumohsin99",
+    imagrUrl: "https://avatars.githubusercontent.com/u/57590526?v=4",
+    userName:"Ata Ul Mohsin"
+  },
+  {
+    gitUrl: "https://github.com/Bsef19a514",
+    imagrUrl: "https://avatars.githubusercontent.com/u/72212932?v=4",
+    userName:"Zain Ahmad"
+  },
+  {
+    gitUrl: "https://github.com/Nitishpal68",
+    imagrUrl: "https://avatars.githubusercontent.com/u/72151454?v=4",
+    userName:"Nitish Pal"
+  },
+
+  {
+    gitUrl: "https://github.com/mursalfk",
+    imagrUrl: "https://avatars.githubusercontent.com/u/36442744?v=4",
+    userName:"Mursal Furqan"
+  },
+  {
+    gitUrl: "https://github.com/elishahyousaf",
+    imagrUrl: "https://avatars.githubusercontent.com/u/65106812?v=4",
+    userName:"Elishah Yousaf"
+  },
+  {
+    gitUrl: "https://github.com/ATTA-E-MUSTAFA",
+    imagrUrl: "https://avatars.githubusercontent.com/u/102528299",
+    userName:"ATTA-E-MUSTAFA"
+  },
+  {
+    gitUrl: "https://github.com/emanmunir",
+    imagrUrl: "https://avatars.githubusercontent.com/u/104212613?v=4",
+    userName:"Eman Munir"
+  },
+  {
+    gitUrl: "https://github.com/ridsashabbir",
+    imagrUrl: "https://avatars.githubusercontent.com/u/81819648?v=4",
+    userName:"Ridsa Shabbir"
+  },
+  {
+    gitUrl: "https://github.com/Myrausman",
+    imagrUrl: "https://avatars.githubusercontent.com/u/99428378?v=4",
+    userName:"Maira Usman"
+  },
+  {
+    gitUrl: "https://github.com/Ameerhamza2000",
+    imagrUrl: "https://avatars.githubusercontent.com/u/92073068?v=4",
+    userName:"Null"
+  },
+  {
+    gitUrl: "https://github.com/nomanlatif123",
+    imagrUrl: "https://avatars.githubusercontent.com/u/75476323?v=4",
+    userName:"Noman Latif"
+  },
+  {
+    gitUrl: "https://github.com/Osama-Sultan189",
+    imagrUrl: "https://avatars.githubusercontent.com/u/59367747?v=4",
+    userName:"Osama Sultan"
+  },
+  {
+    gitUrl: "https://github.com/shubhambaghel34",
+    imagrUrl: "https://avatars.githubusercontent.com/u/21123907?v=4",
+    userName:"Shubham_Baghel"
+  },
+  {
+    gitUrl: "https://github.com/VedantKhairnar",
+    imagrUrl: "https://avatars.githubusercontent.com/u/42309779?v=4",
+    userName:"Vedant Khairnar"
+  },
+  {
+    gitUrl: "https://github.com/Zeeshan6781",
+    imagrUrl: "https://avatars.githubusercontent.com/u/67405028?v=4",
+    userName:"Zeeshan Latif"
+  },
+  {
+    gitUrl: "https://github.com/MubeenAhmad571",
+    imagrUrl: "https://avatars.githubusercontent.com/u/56739319?v=4",
+    userName:"Mubeen Ahmad"
+  },
+  {
+    gitUrl: "https://github.com/unsmuhammad",
+    imagrUrl: "https://avatars.githubusercontent.com/u/107023053?v=4",
+    userName:"Null"
+  },
+  {
+    gitUrl: "https://github.com/Mahnoor786-dev",
+    imagrUrl: "https://avatars.githubusercontent.com/u/80174852?v=4",
+    userName:"Mahnoor Ali"
+  },
+  {
+    gitUrl: "https://github.com/MuhammadSaad000",
+    imagrUrl: "https://avatars.githubusercontent.com/u/67402416?v=4",
+    userName:"Muhammad Saad"
+  },
+  {
+    gitUrl: "https://github.com/gupta-soham",
+    imagrUrl: "https://avatars.githubusercontent.com/u/97831613?v=4",
+    userName:"Soham Gupta"
+  },
+  {
+    gitUrl: "https://avatars.githubusercontent.com/u/67449314?v=4",
+    imagrUrl: "https://avatars.githubusercontent.com/u/67449314?v=4",
+    userName:"USAMA MALOO"
+  },
+  {
+    gitUrl: "https://github.com/Vansh2305",
+    imagrUrl: "https://avatars.githubusercontent.com/u/72547809?v=4",
+    userName:"Null"
+  },
+  {
+    gitUrl: "https://github.com/NoorAtif",
+    imagrUrl: "https://avatars.githubusercontent.com/u/77112713?v=4",
+    userName:"Noor Atif"
+  },
+  {
+    gitUrl: "https://github.com/samadpls",
+    imagrUrl: "https://avatars.githubusercontent.com/u/94792103?v=4",
+    userName:"Abdul Samad Siddiqui"
+  },
+  {
+    gitUrl: "https://github.com/ahmad-14a",
+    imagrUrl: "https://avatars.githubusercontent.com/u/115044686?v=4",
+    userName:"Ahmad Ali"
+  },
+  {
+    gitUrl: "https://github.com/AmeenaAbdullah",
+    imagrUrl: "https://avatars.githubusercontent.com/u/99495118?v=4",
+    userName:"Ameena"
+  },
+  {
+    gitUrl: "https://github.com/Amnaazam123",
+    imagrUrl: "https://avatars.githubusercontent.com/u/71166016?v=4",
+    userName:"Amna Azam"
+  },
+  {
+    gitUrl: "https://github.com/Nitya-Pasrija",
+    imagrUrl: "https://avatars.githubusercontent.com/u/97171261?v=4",
+    userName:"Nitya Pasrija"
+  },
+  {
+    gitUrl: "https://github.com/abdulmoizquddus",
+    imagrUrl: "https://avatars.githubusercontent.com/u/66178373?v=4",
+    userName:"Abdul Moiz Quddus"
+  },{
+    gitUrl: "https://github.com/areebamuzammil",
+    imagrUrl: "https://avatars.githubusercontent.com/u/73306700?v=4",
+    userName:"Areeba Muzammil"
+  },{
+    gitUrl: "https://github.com/CoderSah",
+    imagrUrl: "https://avatars.githubusercontent.com/u/115417419?v=4",
+    userName:"Null"
+  },{
+    gitUrl: "https://github.com/RahulSahOfficial",
+    imagrUrl: "https://avatars.githubusercontent.com/u/62435110?v=4",
+    userName:"Developer Sah"
+  },{
+    gitUrl: "https://github.com/farhanashrafdev",
+    imagrUrl: "https://avatars.githubusercontent.com/u/53370109?v=4",
+    userName:"Farhan Ashraf"
+  },{
+    gitUrl: "https://github.com/fasihullahsaleem",
+    imagrUrl: "https://avatars.githubusercontent.com/u/97796711?v=4",
+    userName:"Fasih Ullah Saleem"
+  },{
+    gitUrl: "https://github.com/gaurav1205",
+    imagrUrl: "https://avatars.githubusercontent.com/u/23035869?v=4",
+    userName:"Gaurav Kumar"
+  },{
+    gitUrl: "https://github.com/JeevanSainik789",
+    imagrUrl: "https://avatars.githubusercontent.com/u/115283993?v=4",
+    userName:"Null"
+  },{
+    gitUrl: "https://github.com/kshitijv256",
+    imagrUrl: "https://avatars.githubusercontent.com/u/101321276?v=4",
+    userName:"Kshitij Verma"
+  },{
+    gitUrl: "https://github.com/laibakamal",
+    imagrUrl: "https://avatars.githubusercontent.com/u/84386567?v=4",
+    userName:"LAIBA KAMAL"
+  },{
+    gitUrl: "https://github.com/RmNj17",
+    imagrUrl: "https://avatars.githubusercontent.com/u/90838977?v=4",
+    userName:"Manoj Khatri"
+  },{
+    gitUrl: "https://github.com/MananDev",
+    imagrUrl: "https://avatars.githubusercontent.com/u/88824142?v=4",
+    userName:"Muhammad Manan"
+  },{
+    gitUrl: "https://github.com/ritam077",
+    imagrUrl: "https://avatars.githubusercontent.com/u/115361914?v=4",
+    userName:"Ritam Kumar Kundu"
+  },{
+    gitUrl: "https://github.com/heysagnik",
+    imagrUrl: "https://avatars.githubusercontent.com/u/70798495?v=4",
+    userName:"Sagnik Sahoo"
+  },{
+    gitUrl: "https://github.com/SaharshDaNerd",
+    imagrUrl: "https://avatars.githubusercontent.com/u/58357403?v=4",
+    userName:"Saharsh Saxena"
+  },{
+    gitUrl: "https://github.com/samsorrahman",
+    imagrUrl: "https://avatars.githubusercontent.com/u/112087807?v=4",
+    userName:"Samsor Rahman"
+  },{
+    gitUrl: "https://github.com/Souvik2376",
+    imagrUrl: "https://avatars.githubusercontent.com/u/100219950?v=4",
+    userName:"Souvik Banerjee"
+  },{
+    gitUrl: "https://github.com/beer-idev",
+    imagrUrl: "https://avatars.githubusercontent.com/u/34055979?v=4",
+    userName:"Beer Idev"
+  },{
+    gitUrl: "https://github.com/canggihwr",
+    imagrUrl: "https://avatars.githubusercontent.com/u/57065088?v=4",
+    userName:"Canggih Wahyu Rinaldi"
+  },{
+    gitUrl: "https://github.com/heyitsuzair",
+    imagrUrl: "https://avatars.githubusercontent.com/u/89875366?v=4",
+    userName:"Muhammad Uzair"
+  },{
+    gitUrl: "https://github.com/iqrasarwar",
+    imagrUrl: "https://avatars.githubusercontent.com/u/71145709?v=4",
+    userName:"Iqra Sarwar"
+  },{
+    gitUrl: "https://github.com/mafzaldev",
+    imagrUrl: "https://avatars.githubusercontent.com/u/78255759?v=4",
+    userName:"Muhammad Afzal"
+  }
+];
+
+export default Contributers;
