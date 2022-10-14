0bf66016a9830ff1c771d335a9ae64f902896eee
Abdul Moiz Quddus <66178373+abdulmoizquddus@users.noreply.github.com>
2022-10-15 01:19:36 +0500

    List Comprehensions
    List Comprehensions (Solved HackerRank Problem)

---
+++ Python/7_List_Comprehensions.py
@@ -0,0 +1,13 @@
+# Name : Abdul Moiz Quddus
+# Date : 10/15/2022
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+newx = [x for x in range(x+1)]
+newy = [x for x in range(y+1)]
+newz = [x for x in range(z+1)]
+xyz = [[x,y,z] for x in newx for y in newy for z in newz if x+y+z != n]
+print(xyz)
