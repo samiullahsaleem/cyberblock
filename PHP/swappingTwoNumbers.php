8540defb9051180df097025d13c6cf03dafba147
hassanaamir12 <80892294+hassanaamir12@users.noreply.github.com>
2022-10-17 18:52:20 +0500

    swappingTwoNumbers.php

---
+++ PHP/swappingTwoNumbers.php
@@ -0,0 +1,17 @@
+<?php
+
+    $a = 45;
+    $b = 78;
+
+    echo "Before swapping";
+    echo "a =".$a."  b=".$b;
+
+    // Swapping Logic
+    $third = $a;
+    $a = $b;
+    $b = $third;
+
+    echo "After swapping";
+    echo "a =".$a."  b=".$b;
+    
+?>
