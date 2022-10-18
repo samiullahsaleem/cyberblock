fa1f94c2f7e97fdc39364e8d99a754caf508d894
abubaker9k <muhammad.abubaker350@gmail.com>
2022-10-18 17:43:08 +0500

    bitcoin.py

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -5,9 +5,10 @@ b = input("What do you do for fun?")
 print("So your hobbies are {}".format(b))
 c = input("What is your university name? ")
 print("So your university name is {}".format(c))
+sum(2,3)
 def sum(a,b,c):
     a = a + b
     c = b
     print(a)
+    exit()
 
-sum(2,3,0)
