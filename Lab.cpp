7b00cf38317391f7f47780eabaa90d6df97e33c1
Asra Mushtaq <115940183+asragithubit@users.noreply.github.com>
2022-10-20 01:09:07 +0500

    Number of objects created in memory

---
+++ Lab.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//using namespace std;
+//class ObjectTracker
+//{
+//private:
+//	static int counter;
+//public:
+//	ObjectTracker();
+//	~ObjectTracker();
+//	int getCounter();
+//};
+//int ObjectTracker::counter = 0;
+//ObjectTracker::ObjectTracker()
+//{
+//	counter++;
+//}
+//ObjectTracker::~ObjectTracker()
+//{
+//	counter--;
+//}
+//int ObjectTracker::getCounter()
+//{
+//	return counter;
+//}
+//int main()
+//{
+//	ObjectTracker obj1;
+//	ObjectTracker obj2;
+//	ObjectTracker obj3;
+//	cout << "Total Objects : " << obj3.getCounter();
+//	return 0;
+//}
