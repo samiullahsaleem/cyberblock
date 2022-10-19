6035b964947cd840fdf838cb70e10d7cdb95da28
ahmadnisarr <115939371+ahmadnisarr@users.noreply.github.com>
2022-10-19 11:28:25 -0700

    check number power of 2
    by Bit manipulation,check number is the power of 2

---
+++ back.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+//using namespace std;
+//bool isPower(int n)
+//{
+//	return (n && !(n & n - 1));
+//}
+//int main()
+//{
+//	cout << "Enter a number: "; int n; cin >> n;
+//	cout << isPower(n);// if 1 return than answer is true,if 0 than answer is false
+//}
