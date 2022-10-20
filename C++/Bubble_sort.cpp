f62088b6bc01159591a5e6c26b15a9d4d4cd1767
AlsaMunir <115931519+AlsaMunir@users.noreply.github.com>
2022-10-20 17:43:46 +0500

    Update Bubble_sort.cpp

--- C++/Bubble_sort.cpp
+++ C++/Bubble_sort.cpp
@@ -1,9 +1,9 @@
 #include < iostream >
   using namespace std;
 
-void bubbleSort(int a[]) {
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < (5 - i - 1); j++) {
+void bubbleSort(int *a,int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < (size- 1); j++) {
       if (a[j] > a[j + 1]) {
         int temp = a[j];
         a[j] = a[j + 1];
@@ -14,21 +14,24 @@ void bubbleSort(int a[]) {
 }
 
 int main() {
-  int myarray[5];
   int size;
-  cout << "Enter 5 integers in any order: " << endl;
-  for (int i = 0; i < 5; i++) {
+  cout<<"enter size:"<<endl;
+  cin>>size;
+  //dynamically allocating array
+  int *myarray=new int[size];
+ cout << "Enter integers in any order: " << endl;
+  for (int i = 0; i < size; i++) {
     cin >> myarray[i];
   }
   cout << "Before Sorting" << endl;
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < size; i++) {
     cout << myarray[i] << " ";
   }
 
-  bubbleSort(myarray); // sorting
+  bubbleSort(myarray,size); // sorting
 
   cout << endl << "After Sorting" << endl;
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < size; i++) {
     cout << myarray[i] << " ";
   }
 
