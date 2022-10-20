eb01e319b97beed5617e39949fba2cc4f71c9f9b
AreebaQamer <115891143+AreebaQamer@users.noreply.github.com>
2022-10-20 08:02:32 -0700

    Update Sum Of Left Diagonal.cpp

--- Sum Of Left Diagonal.cpp
+++ Sum Of Left Diagonal.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
 using namespace std;
-const int ROWS = 3;
-const int COLS = 3; 
+ int rows,col;
+cout<<"enter the number of rows:"<<endl;
+cin>>rows;
+cout<<"enter the number of columns:"<<endl;
+cin>>col;
 int main()
 {
-	int arr[ROWS][COLS],sum = 0;
+	int arr[rows][col],sum = 0;
 	cout << "Enter the Nmubers Of Diagonal:" << endl;
-	for (int i = 0; i < ROWS; i++)
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 0; j < COLS;j++)
+		for (int j = 0; j < col;j++)
 		{
 			cin >> arr[i][j];
 		}
 	}
 	cout <<"Diagonal Matrix is:" << endl;
-	for (int i = 0; i < ROWS; i++)
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 0; j < COLS;j++)
+		for (int j = 0; j < cols;j++)
 		{
 			cout<<arr[i][j]<<" ";
 		}
 		cout << endl;
 	}
-	for (int i = 0; i < ROWS; i++)
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 0; j < COLS;j++)
+		for (int j = 0; j < col;j++)
 		{
 			if (i == j)
 			{
