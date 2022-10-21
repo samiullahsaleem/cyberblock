d23d34e83f40e054ea76737cb965937a049a2e12
OSMAN ALI <osmanali6263@gmail.com>
2022-10-22 04:24:41 +0500

    Added fast solution of 3 problems of leetcode.

---
+++ C++/Fast Solution of 3 Problems/TwoSum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> re;
+        int i =0, j=nums.size()-1;
+        while(i<j){
+            if(nums[i]+ nums[j]< target){
+                i++;
+            }
+            else if(nums[i] + nums[j]> target){
+                j--;
+            }
+            else if(nums[i] + nums[j]== target){
+                re.push_back(i);
+                re.push_back(j);
+                return re;
+            }
+        }
+        return re;
+        
+    }
+};
+
+
+int main(){
+
+    Solution s;
+    vector<int> ss{2,7,11,15};
+    vector<int> re = s.twoSum(ss, 9);
+    for(int i=0; i<2; i++){
+        cout<< re[i] << " ";
+    }
+    return 0;
+}
