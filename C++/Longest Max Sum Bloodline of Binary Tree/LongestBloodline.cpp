b809bb067a9d39e3934b90dd23df0486ad0c2354
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-10-22 20:17:31 +0530

    added a famous Medium level Tree qna
    Q:Given a binary tree of size N. Your task is to complete the function sumOfLongRootToLeafPath(), that find the sum of all nodes on the longest path from root to leaf node.
    If two or more paths compete for the longest path, then the path having maximum sum of nodes is being considered.
    
    I've given the solution to the following problem in the CPP folder in a folder titled: Longest Max Sum Bloodline of Binary Tree

---
+++ C++/Longest Max Sum Bloodline of Binary Tree/LongestBloodline.cpp
@@ -0,0 +1,142 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+class Solution
+{
+public:
+    void solve(Node* node, int len, int sum, int &maxSum, int & maxLen)
+    {
+        // base
+        if(node == NULL) 
+        {
+            if(len>maxLen)
+            {
+                maxLen=len;
+                maxSum=sum;
+            }
+            else if (len==maxLen)
+            {
+                maxSum = max(maxSum,sum);
+            }
+            return;
+        }
+        sum=sum+node->data;
+        solve(node->left,len+1,sum,maxSum,maxLen);
+        solve(node->right,len+1,sum,maxSum,maxLen);
+        
+    }
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        int maxSum=INT_MIN;
+        int maxLen=0;
+        solve(root,0,0,maxSum,maxLen);
+        return maxSum;
+    }
+};
+
+
+
+
+int main() {
+    int tc;
+    cin>>tc;
+    cin.ignore(256, '\n');
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        vector<int> vec = ob.topView(root);
+        for(int x : vec)
+            cout<<x<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
