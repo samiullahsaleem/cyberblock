4294b9a235641b70df2e5df42cab82e7efdacb4d
Syed Muhammad Saad <syed.07saad@gmail.com>
2022-10-25 16:09:13 +0500

    add JS crashCourse, some exciting c++ & c# codes

---
+++ C#/loadHTML_fromWeb/Q1/Program.cs
@@ -0,0 +1,42 @@
+﻿                                            // ************ 19k-0209 ***********
+                                           // Using HtmlAgilityPack, scrap the Html doc from the Web.
+                                          // Then just save in the file with .html extension.
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter URL: ");
+            var url = Console.ReadLine();
+
+            Console.WriteLine("Enter Folder: ");
+            var folder = Console.ReadLine();
+
+            GetHTML(url, folder);
+            Console.ReadLine();
+        }
+        private static async void GetHTML(string p_url, string folder)
+        {
+            string url = p_url;
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            Console.WriteLine(html);
+
+            string path = folder + "data.html";
+            StreamWriter saveHtml = new StreamWriter(path, true);
+            saveHtml.WriteLine(html);
+            saveHtml.Dispose();
+        }
+    }
+}
